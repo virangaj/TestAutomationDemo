9c98749f8f131d65075cec45a175e61906b76e82
Viranga Jayawardana <virangapasindu4@gmail.com>
2023-07-12 22:09:13 +0530

    login page test complete

--- src/main/java/com/automationdemo/pages/TasksPage.java
+++
@@ -1,9 +0,0 @@
-package com.automationdemo.pages;
-
-import com.automationdemo.base.TestBase;
-
-public class TasksPage extends TestBase {
-
-	
-	
-}
--- src/main/java/com/automationdemo/pages/UsersPage.java
+++
@@ -1,7 +0,0 @@
-package com.automationdemo.pages;
-
-import com.automationdemo.base.TestBase;
-
-public class UsersPage extends TestBase  {
-
-}
